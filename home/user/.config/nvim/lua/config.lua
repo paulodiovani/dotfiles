9d0f08e776a1cc07885600ef1a4e9d8aa2233476
Paulo Diovani <paulo@diovani.com>
2022-06-17 16:02:13 -0300

    Enable linter for react files

--- home/user/.config/nvim/lua/config.lua
+++ home/user/.config/nvim/lua/config.lua
@@ -6,7 +6,9 @@
 vim.diagnostic.config({ virtual_text = false })
 require('lint').linters_by_ft = {
   javascript = {'eslint'},
+  javascriptreact = {'eslint'},
   typescript = {'eslint'},
+  typescriptreact = {'eslint'},
   ruby = {'ruby'}
 }
 
