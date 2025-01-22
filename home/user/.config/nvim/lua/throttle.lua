e4fe6bb9cae4a051207775c6cc3f1360ed848082
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2025-01-22 11:05:57 -0300

    Revert "Disable hover for ruff_lsp"
    This reverts commit f4c82c18e77de84b0cf3021165b8374d5bd6d62e.

--- home/user/.config/nvim/lua/throttle.lua
+++ home/user/.config/nvim/lua/throttle.lua
@@ -1,7 +1,3 @@
---- Throttles a function so that it can only be called once within a specified delay.
--- @param callback The function to be throttled.
--- @param delay The delay in milliseconds during which the function cannot be called again.
--- @return A new function that wraps the original function with throttling behavior.
 local function throttle(callback, delay)
     local is_throttled = false
     local timer = vim.loop.new_timer()
