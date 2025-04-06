fb75aeb758765093d6255d7788c86cacd8284161
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2025-04-06 20:02:17 -0300

    Move plugins to lazy.vim

---
+++ home/user/.config/nvim/lua/plugins/file-drawer.lua
@@ -0,0 +1,7 @@
+return {
+  "kyazdani42/nvim-tree.lua",
+  dependencies = { "kyazdani42/nvim-web-devicons" },
+  config = function()
+    require("file-drawer")
+  end,
+}
---
+++ home/user/.config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,18 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.diagnostics.stylelint,
+        null_ls.builtins.diagnostics.yamllint,
+        null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "markdown" } }),
+        null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "jsonc" }, extra_args = { "--parser=json" } }),
+        null_ls.builtins.formatting.erb_format,
+        null_ls.builtins.formatting.stylelint,
+      },
+    })
+  end,
+}
