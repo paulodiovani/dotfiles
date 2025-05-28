d8753ab07a01c0d3514a5bb939ca9d9da06009c3
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2025-05-28 11:56:01 -0300

    Fix search commands

--- home/user/.config/nvim/lua/plugins/fzf.lua
+++ home/user/.config/nvim/lua/plugins/fzf.lua
@@ -26,30 +26,32 @@ return {
 
     -- Use Rgh command to search hidden files
     vim.api.nvim_create_user_command('Rgh', function(opts)
-      require('fzf-lua').grep({ search = opts.args, rg_opts =
-      '--hidden --column --line-number --no-heading --color=always --smart-case' })
+      require('fzf-lua').grep({
+        search = opts.args,
+        rg_opts =
+        '--hidden --column --line-number --no-heading --color=always --smart-case'
+      })
     end, { nargs = '*' })
   end,
 
   cmd = {
     'FzfLua',
-    'Rg',
-    'Rgh',
   },
 
   keys = {
-    { '<C-p>',      '<Cmd>FzfLua git_files<CR>',              mode = { 'n' } },
-    { '<Leader>p',  '<Cmd>FzfLua git_files<CR>',              mode = { 'n' } },
-    { '<Leader>P',  '<Cmd>FzfLua files<CR>',                  mode = { 'n' } },
-    { '<Leader>b',  '<Cmd>FzfLua buffers<CR>',                mode = { 'n' } },
-    { '<Leader>t',  '<Cmd>FzfLua btags<CR>',                  mode = { 'n' } },
-    { '<Leader>T',  '<Cmd>FzfLua tags<CR>',                   mode = { 'n' } },
-    { '<Leader>l',  '<Cmd>FzfLua blines<CR>',                 mode = { 'n' } },
-    { '<Leader>L',  '<Cmd>FzfLua lines<CR>',                  mode = { 'n' } },
-    { '<Leader>m',  '<Cmd>FzfLua marks<CR>',                  mode = { 'n' } },
-    { '<Leader>rg', 'yiw<Cmd>FzfLua grep_cword<CR>',          mode = { 'n' } },
-    { '<Leader>rg', 'y<Cmd>FzfLua grep_visual<CR>',           mode = { 'v' } },
-    { '<Leader>rh', 'yiw<Cmd>FzfLua grep_cword --hidden<CR>', mode = { 'n' } },
-    { '<Leader>rh', 'y<Cmd>FzfLua grep_visual --hidden<CR>',  mode = { 'v' } },
+    { '<C-p>',      '<Cmd>FzfLua git_files<CR>',   mode = { 'n' } },
+    { '<Leader>p',  '<Cmd>FzfLua git_files<CR>',   mode = { 'n' } },
+    { '<Leader>P',  '<Cmd>FzfLua files<CR>',       mode = { 'n' } },
+    { '<Leader>b',  '<Cmd>FzfLua buffers<CR>',     mode = { 'n' } },
+    { '<Leader>t',  '<Cmd>FzfLua btags<CR>',       mode = { 'n' } },
+    { '<Leader>T',  '<Cmd>FzfLua tags<CR>',        mode = { 'n' } },
+    { '<Leader>l',  '<Cmd>FzfLua blines<CR>',      mode = { 'n' } },
+    { '<Leader>L',  '<Cmd>FzfLua lines<CR>',       mode = { 'n' } },
+    { '<Leader>m',  '<Cmd>FzfLua marks<CR>',       mode = { 'n' } },
+    -- search word under cursor or selection
+    { '<Leader>rg', '<Cmd>FzfLua grep_cword<CR>',  mode = { 'n' } },
+    { '<Leader>rg', '<Cmd>FzfLua grep_visual<CR>', mode = { 'v' } },
+    { '<Leader>rh', 'yiw:Rgh <C-r>"<CR>',          mode = { 'n' } },
+    { '<Leader>rh', 'y:Rgh <C-r>"<CR>',            mode = { 'v' } },
   },
 }
