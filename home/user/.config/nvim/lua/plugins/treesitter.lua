9f0b524ba3eb2f2a421f22d2f0e613632dcf353b
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2025-06-04 11:09:05 -0300

    Update nvim-treesitter config

--- home/user/.config/nvim/lua/plugins/treesitter.lua
+++ home/user/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "json",
-        "lua",
-        "luadoc",
-        "markdown",
-        "vim",
-        "vimdoc",
-      },
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+
+  opts = {
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+    ensure_installed = {
+      "bash",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "vim",
+      "vimdoc",
+    },
+    highlight = {
+      enable = true,
+    },
+  },
 }
