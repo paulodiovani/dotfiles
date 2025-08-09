e550e1e7be970e12606a339a5581dda2ade34fab
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2025-08-09 15:04:56 -0300

    Restore colorscheme overrides

--- home/user/.config/nvim/lua/plugins/colorscheme.lua
+++ home/user/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,19 +11,19 @@ return {
 
     -- fix colorschemes comment issues
     -- https://github.com/tinted-theming/tinted-vim/issues/103
-    -- vim.cmd([[
-    --   function! s:tinted_customize() abort
-    --     call Tinted_Hi('@variable.member', g:tinted_gui08, '', g:tinted_cterm08, '', '', '')
-    --   endfunction
-    --
-    --   " fix colorscheme on startup
-    --   call s:tinted_customize()
-    --
-    --   " fix colorcheme on theme change
-    --   augroup on_change_colorschema
-    --     autocmd!
-    --     autocmd ColorScheme * call s:tinted_customize()
-    --   augroup END
-    -- ]])
+    vim.cmd([[
+      function! s:tinted_customize() abort
+        call Tinted_Hi('@variable.member', g:tinted_gui08, '', g:tinted_cterm08, '', '', '')
+      endfunction
+
+      " fix colorscheme on startup
+      call s:tinted_customize()
+
+      " fix colorcheme on theme change
+      augroup on_change_colorschema
+        autocmd!
+        autocmd ColorScheme * call s:tinted_customize()
+      augroup END
+    ]])
   end
 }
