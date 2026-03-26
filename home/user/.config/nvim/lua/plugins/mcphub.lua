483a0f8ac99fb849acbf204dcb710ba309519dd8
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2026-03-26 12:50:19 -0300

    Allow code Companion to use claude code mcp config

--- home/user/.config/nvim/lua/plugins/mcphub.lua
+++ home/user/.config/nvim/lua/plugins/mcphub.lua
@@ -6,5 +6,9 @@ return {
   build = "bundled_build.lua",
   opts = {
     use_bundled_binary = true,
+    workspace = {
+      enabled = true,
+      look_for = { ".mcphub/servers.json", ".mcp.json", ".vscode/mcp.json", ".cursor/mcp.json" },
+    },
   },
 }
